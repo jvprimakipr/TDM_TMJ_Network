2615a0b71bcb21adceb96e71f8943795e85ca41c
jvprimakipr <j.joaoviniciuspr@hotmail.com>
2021-07-14 21:23:03 -0300

    Update characters

--- Code/Functions/class_characters.py
+++ Code/Functions/class_characters.py
@@ -1,26 +1,65 @@
 class characters:
     def __init__(self):
         # auxiliares para contagem
+        self.figurante_number = 0
         self.atendent_number = 0
         self.father_number = 0
-        self.mother_number = 0
-        self.figurante_number = 0
+        self.mother_number = 0        
         self.child_number = 0
         self.comic_book = ''
 
+        # God
         self.mau = 'Maurício de Souza'
         
-        # TDM
+        ###############################
+        ####### TURMA PRINCIPAL #######
+        ###############################
+        
+        # Protagonistas
         self.mon = 'Mônica'
         self.ceb = 'Cebolinha'
-        self.cas = 'Cascão'
         self.mag = 'Magali'
+        self.cas = 'Cascão'
+        
+        # Pets
+        self.san = 'Sansão'
         self.moca = 'Monicão'
         self.floq = 'Floquinho'
-        self.chov = 'Chovenista'
         self.min = 'Mingau'
-        self.xav = 'Xaveco'
+        self.chov = 'Chovinista'
+        self.bid = 'Bidu'
+        self.xim = 'Ximbuca'
+        self.bor = 'Bóris'
+        self.manf = 'Manfredo'
+        
+        # Secundários (Masculino)
         self.franj = 'Franjinha'
+        self.xav = 'Xaveco'
+        self.con = 'Do Contra'
+        self.nim = 'Nimbus'
+        self.dud = 'Dudu'
+        self.luc = 'Luca'
+        self.hum = 'Humberto'
+        self.tit = 'Titi'
+        self.jer = 'Jeremias'
+        self.man = 'Manezinho'
+        self.anj = 'Anjinho'
+        self.quin = 'Quinzinho'
+        self.zlui = 'Zé Luís'
+        self.blo = 'Bloguinho'
+        self.tv = 'Teveluisão'
+        
+        # Secundárias (Feminino)
+        self.den = 'Denise'
+        self.mar = 'Marina'
+        self.marceb = 'Maria Cebolinha'
+        self.carfru = 'Carminha Frufru'
+        self.marcas = 'Maria Cascuda'
+        self.xab = 'Xabéu'
+        self.ani = 'Aninha'
+        self.dor = 'Dorinha'
+        
+        # Pais e Mães
         self.mmon = 'Dona Luísa'
         self.pmon = 'Seu Souza'
         self.mceb = 'Dona Cebola'
@@ -29,79 +68,144 @@ class characters:
         self.pmag = 'Seu Carlito'
         self.mcas = 'Dona Lurdinha'
         self.pcas = 'Seu Antenor'
-        self.san = 'Sansão'
-        self.bid = 'Bidu'
-        self.luc = 'Luca'
-        self.den = 'Denise'
+        self.mxav = 'Dona Xarlene'
         self.pxav = 'Seu Xavier'
-        self.mar = 'Marina'
-        self.viv = 'Bruxa Viviane'
-        self.bor = 'Bóris'
-        self.dud = 'Dudu'
         self.mdud = 'Dona Cecília'
-        self.marceb = 'Maria Cebolinha'
-        self.con = 'Do Contra'
-        self.hum = 'Humberto'
-        self.manf = 'Manfredo'
-        self.tv = 'Teveluisão'
-        self.blo = 'Bloguinho'
+        self.pdud = 'Seu Durval'
         self.mtv = 'Mãe do Teveluisão'
-        self.nim = 'Nimbus'
-        self.jer = 'Jeremias'
-        self.carfru = 'Carminha Frufru'
-        self.anj = 'Anjinho'
-        self.spa = 'São Paulo'
-        self.quin = 'Quinzinho'
+        
+        # Tios(as), Avôs(ós)
+        self.tnen = 'Tia Nena
+        self.tpep = 'Tio Pepo'
+        self.vxep = 'Vó Xepa'
+        
+        # Adultos (em geral)
         self.lou = 'Louco'
+        self.juc = 'Seu Juca'
+        self.viv = 'Bruxa Viviane'
+        self.spa = 'São Paulo'
+        
+        # Vida Real
         self.ali = 'Alice'
-        self.lui = 'Zé Luís'
-
-        # turma do Penadinho
+        
+        ###############################
+        ##### TURMA DO PENADINHO ######
+        ###############################
+        
         self.pen = 'Penadinho'
-        self.fra = 'Frank'
-        self.mum = 'Muminho'
-        self.vam = 'Zé Vampir'
-        self.lob = 'Lobi'
         self.alm = 'Alminha'
-        self.cav = 'Zé Caveirinha'
+        self.mum = 'Muminho'
+        self.zvam = 'Zé Vampir'
+        self.fra = 'Frank'
+        self.lob = 'Lobi'
         self.cra = 'Cranicóla'
+        self.zcav = 'Zé Caveirinha'
         self.mor = 'Dona Morte'
+        self.ceg = 'Dona Cegonha'
 
-        # chico bento
+        ###############################
+        #### TURMA DO CHICO BENTO #####
+        ###############################
+        
+        # Crianças
         self.chi = 'Chico Bento'
         self.ros = 'Rosinha'
-        self.pro = 'Dona Marocas'
-        self.pad = 'Padre Lino'
-        self.mchi = 'Dona Cotinha'
-        self.nho = 'Nhô Lau'
-        self.lel = 'Zé Lelé'
-        self.mlel = 'Dona Lalá'
+        self.zlel = 'Zé Lelé'
+        self.marcaf = 'Maria Cafufa'
+        self.zroc = 'Zé da Roça'
+        self.hir = 'Hiro'
+        self.pzec = 'Primo Zeca'
         self.gen = 'Genesinho'
+        
+        # Adultos
+        self.mchi = 'Dona Cotinha'
+        self.pchi = 'Nhô Bento'
+        self.mlel = 'Dona Lalá'
+        self.plel = 'Seu Leocádio'
+        self.prof = 'Dona Marocas'
+        self.pad = 'Padre Lino'
+        self.nho = 'Nhô Lau'
+        self.vdit = 'Vó Dita'
+        
+        # Animais
+        self.fid = 'Fido'
+        self.tor = 'Torresmo'
+        self.gis = 'Giselda'
+        self.mim = 'Mimosa'
 
-        # tina
-        self.rol = 'Rolo'
+        ###############################
+        ######## TURMA DA TINA ########
+        ###############################
+        
         self.tin = 'Tina'
+        self.rol = 'Rolo'
         self.pip = 'Pipa'
+        self.zec = 'Zecão'
+        self.pat = 'Patricinha'
         self.ser = 'Serginho'
+        self.ton = 'Toneco'
+        self.vov = 'Vovoca'
 
-        # piteco
+        ###############################
+        ## TURMA DO PITECO E HORÁCIO ##
+        ###############################
+        
+        # Humanos
         self.pit = 'Piteco'
-        self.var = 'Vartolo'
+        self.thu = 'Thuga'
         self.bol = 'Bolota'
+        self.ogr = 'Ogra'
+        self.bel = 'Beleléu'
+        self.var = 'Vartolo'
 
-        # astronauta
+        # Dinossauros
+        self.hor = 'Horácio'
+        self.luc = 'Lucinda'
+        self.tec = 'Tecodonte'
+        self.sim = 'Simone'
+        
+        ###############################
+        #### TURMA DO AASTRONAUTA #####
+        ###############################
+        
         self.ast = 'Astronauta'
+        self.rit = 'Ritinha'
         self.mast = 'Natalina'
         self.past = 'Astrogildo'
 
-        # papa-capim
+        ###############################
+        ##### TURMA DO PAPA CAPIM #####
+        ###############################
+        
         self.pap = 'Papa-Capim'
+        self.caf = 'Cafuné'
+        self.jur = 'Jurema'
         self.cai = 'Caiapopó'
         self.paj = 'Pajé'
         self.cac = 'Cacique'
 
-        # turma da mata
+        ###############################
+        ######## TURMA DA MATA ########
+        ###############################
+        
         self.jot = 'Jotalhão'
+        self.rap = 'Raposão'
+        self.coe = 'Coelho Caolho'
+        self.tar = 'Tarugo'
+        self.naj = 'Rita Najura'
+        self.rei = 'Rei Leonino'
+        self.cax = 'Luís Caxeiro'
+    
+    def fig(self, reset = False, n = 0, add = 0):
+        if n != 0:
+            return f'Figurante {n} - {self.comic_book}'
+        elif reset:
+            self.figurante_number = 0
+        elif add != 0:
+            return f'Figurante {self.child_number + add} - {self.comic_book}'
+        else:
+            self.figurante_number += 1
+            return f'Figurante {self.figurante_number + add} - {self.comic_book}'
     
     def atend(self, reset = False, n = 0, add = 0):
         if n != 0:
@@ -135,17 +239,6 @@ class characters:
         else:
             self.mother_number += 1
             return f'Mãe {self.mother_number + add} - {self.comic_book}'
-
-    def fig(self, reset = False, n = 0, add = 0):
-        if n != 0:
-            return f'Figurante {n} - {self.comic_book}'
-        elif reset:
-            self.figurante_number = 0
-        elif add != 0:
-            return f'Figurante {self.child_number + add} - {self.comic_book}'
-        else:
-            self.figurante_number += 1
-            return f'Figurante {self.figurante_number + add} - {self.comic_book}'
     
     def child(self, reset = False, n = 0, add = 0):
         if n != 0:
