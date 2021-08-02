1f77bd257b9c67a507b9d1767f4ca4b2067ac634
IgorMichels <igorxadrezbr@gmail.com>
2021-08-02 20:24:01 -0300

    added comic books 16, 17 and 18

--- Code/TDM/TDM16.py
+++ Code/TDM/TDM16.py
@@ -5,4 +5,69 @@ c = characters()
 c.reset()
 c.comic_book = 'gibi 16'
 
-pages16 = []
+pages16 = [[c.beg, c.chi, c.child(), 'Espírito do Milharal', c.end],
+		   [],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.beg, c.chi, 'Borboleta 1', 'Borboleta 2', 'Pássaro 1', 'Pássaro 2', 'Pássaro 3', 'Coelho', 'Formiga'],
+		   [c.chi, c.nho],
+		   [c.chi, c.nho],
+		   [c.chi, 'Espírito do Milharal'], # Espírito do Milharal só em um balão de fala
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal', 'Pássaro 4', 'Pássaro 5'] + c.shades(3),
+		   [c.chi, 'Espírito do Milharal'], # ambos só em balão de fala
+		   [c.chi, 'Espírito do Milharal', c.fat(), c.mot(), c.mot(), c.att()] + c.kids(3) + c.shades(14),
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal', c.att()] + c.kids(4),
+		   [c.chi, 'Espírito do Milharal', c.child(), c.att(add = 0)],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, 'Espírito do Milharal', c.fig()],
+		   [c.chi, 'Espírito do Milharal', c.fig()], # figurante só em balão de fala (motorista do ônibus falando da próxima parada)
+		   [c.chi, 'Espírito do Milharal'],
+		   [c.chi, c.nho],
+		   [c.chi, c.nho, c.end],
+		   [],
+		   [c.beg, c.bid],
+		   [c.bid, c.duq, 'Zé Gordão', 'Dálmata'], # conferir os dois últimos
+		   [c.bid, c.duq, 'Zé Gordão', 'Dálmata', c.end],
+		   [], # [c.tin, c.pip, c.rol, c.zec] # passatempo
+		   [], # [c.ceb, c.mon, c.anj] # correio
+		   [], # [c.tit, c.mon, c.ceb, c.den] # correio (conferir denise)
+		   [],
+		   [c.beg, c.dor, c.mdor],
+		   [c.dor, c.mdor],
+		   [c.dor, c.mdor, c.mag, c.mmag, c.end],
+		   [c.beg, c.cas],
+		   [c.cas, c.end],
+		   [c.beg, c.mag, c.min], # mingau só em balão de fala
+		   [c.mag, c.min, c.end],
+		   [c.beg, c.pap, c.caf, c.fig()],
+		   [c.fig(add = 0), c.jur, 'Indiazinha 1', 'Índio', 'Índia', c.fig(), 'Indiazinha 2', 'Indiazinha 3', c.end],
+		   [],
+		   [c.beg, c.cas, c.ceb, c.doc, c.end],
+		   [c.beg, c.fra, c.fig(), c.fig(), 'Ratinho'],
+		   [c.fra, c.fig(add = 0), c.fig(add = -1), 'Ratinho'],
+		   [c.fra, c.fig(), c.pen],
+		   [c.fra, c.fig(add = 0), c.pen],
+		   [c.fra, c.fig(), c.zvam],
+		   [c.fra, c.fig(add = 0), c.zvam, c.lob, c.fig()],
+		   [c.fra, c.fig(add = 0), c.lob],
+		   [c.fra, c.fig(), c.fig(), c.cra, c.zcav, c.mum],
+		   [c.fra, c.fig(add = 0), c.fig(add = -1), c.fig(), c.fig()],
+		   [c.fra] + c.figs(5) + [c.end],
+		   [],
+		   [c.beg, c.chi, c.nho],
+		   [c.chi, c.nho],
+		   [c.chi, c.nho],
+		   [c.chi, c.nho],
+		   [c.chi, c.nho, 'Boi'],
+		   [c.chi, c.nho, c.fig(), c.fig()],
+		   [c.chi, c.nho, c.fig(add = 0), c.fig(add = -1), c.end],
+		   [c.beg, c.cra, 'Fantasminha', c.end]]
--- Code/TDM/TDM18.py
+++ Code/TDM/TDM18.py
@@ -5,4 +5,71 @@ c = characters()
 c.reset()
 c.comic_book = 'gibi 18'
 
-pages18 = []
+pages18 = [[c.beg, c.spa, c.mon, c.mag, c.cas, c.ceb, c.san, c.end],
+		   [],
+		   [c.mon, c.mag, c.cas, 'Neto do Franjinha'],
+		   [c.beg, c.mag, c.franj]
+		   [c.mag, c.franj, 'Fran J. Inha', 'Monika Bond', 'Maga Rári'], # os três últimos só em imagem no computador
+		   [c.mag, c.franj, 'Monika Bond', 'Maga Rári'], # as duas últimas na tela do computador (imagem)
+		   [c.mag, 'Monika Bond', 'Robô carregando malas 1', 'Robô carregando malas 2', 'Abelha', 'Bidutronic'],
+		   [c.mag, 'Monika Bond', 'Abelha', 'Bidutronic', c.spa, c.san, 'Robô amarrando cadarço', c.fig()], # coelho é o sansão, mas no futuro
+		   [c.mag, 'Monika Bond', 'Abelha', 'Bidutronic', c.att(), c.att()], # os atendentes são robôs
+		   [c.mag, 'Monika Bond', 'Abelha', 'Bidutronic', c.att(), c.att(), c.child(), c.child(), c.mot(), c.fat()], # rt para os atendentes
+		   [c.mag, 'Monika Bond', 'Abelha', 'Bidutronic', c.spa, c.san],
+		   [c.mag, 'Monika Bond', 'Abelha', 'Bidutronic', c.san, c.franj, 'Fran J. Inha'], # franjinha só em imagem no pc
+		   ['Fran J. Inha', 'C-bola'],
+		   [c.mag, 'Monika Bond', 'Bidutronic', 'Fran J. Inha', 'C-bola'],
+		   [c.mag, 'Monika Bond', 'Bidutronic', 'Fran J. Inha', 'C-bola', 'Cecê-boy'],
+		   [c.mag, 'Monika Bond', 'Bidutronic', 'Fran J. Inha', 'C-bola', 'Cecê-boy', c.spa, c.san],
+		   [c.mag, 'Monika Bond', 'Fran J. Inha', 'C-bola', 'Cecê-boy', c.spa, c.san],
+		   [c.mag, 'Monika Bond', 'Bidutronic', 'Fran J. Inha', 'C-bola', 'Cecê-boy'],
+		   [c.mag, 'Monika Bond', 'C-bola', 'Cecê-boy'],
+		   [c.mag, 'Monika Bond', 'C-bola', 'Cecê-boy', c.spa],
+		   [c.mag, 'Monika Bond', 'C-bola', 'Cecê-boy', c.spa],
+		   [c.mag, 'Monika Bond', 'C-bola', 'Cecê-boy', c.san],
+		   ['Monika Bond', c.san, 'Congue-Congue'],
+		   ['C-bola', 'Invasores do Espaço'], # 8 invasores e uma nava mãe
+		   ['Cecê-boy', 'Sapinho'] + c.figs(6),
+		   [c.mag, 'Maga Rári', 'Fantasmildos'], # 3 fantasmas
+		   ['Monika Bond', 'Congue-Congue', c.san],
+		   ['C-bola', 'Etê da Nava Mãe', 'Invasores do Espaço'],
+		   ['Cecê-boy', 'Sapinho', c.fig(), c.fig()],
+		   [c.mag, 'Maga Rári', 'Fantasmildos'],
+		   [c.spa, c.san, 'Monika Bond'],
+		   [c.spa, c.san, 'Monika Bond', 'C-bola', 'Invasores do Espaço'],
+		   ['Cecê-boy', 'Maga Rári', c.mag, c.spa, 'Fantasmildos'],
+		   [c.mag, c.spa, 'Cecê-boy', 'C-bola', 'Maga Rári', 'Monika Bond'],
+		   [c.mag, c.spa, 'Cecê-boy', 'C-bola', 'Maga Rári', 'Monika Bond'],
+		   [c.mag, c.spa, 'Cecê-boy', 'C-bola', 'Maga Rári', 'Monika Bond', 'Bidutronic', 'Fran J. Inha'],
+		   [c.mag, c.franj, 'Cecê-boy', 'C-bola', 'Maga Rári', 'Monika Bond', 'Bidutronic', 'Fran J. Inha'],
+		   [c.mag, c.franj, c.cas, c.ceb, c.mon, c.bid, c.end],
+		   [],
+		   [], # [c.tin, c.cas] # correio
+		   [], # [c.mon, c.ceb, c.mag, c.marcaf] # correio
+		   [], # [c.naj, c.mon] # passatempo
+		   [],
+		   [c.beg, c.chi, 'Onça'],
+		   [c.chi, 'Onça', 'Jacaré', c.end],
+		   [c.beg, c.ceb, c.anj, 'Anjo da Guarda do Anjinho', c.end],
+		   [],
+		   [c.beg, c.mon, c.san, c.ceb, c.cas],
+		   [c.mon, c.san, c.ceb, c.cas, 'Cipolino', 'Gastão'],
+		   [c.mon, c.ceb, c.cas, 'Cipolino', 'Gastão'],
+		   [c.mon, c.ceb, c.cas, 'Cipolino', 'Gastão'],
+		   [c.mon, c.ceb, c.cas, 'Cipolino', 'Gastão', c.san, c.end],
+		   [c.beg, c.pit],
+		   [c.pit, c.zcav, 'Dinossauro Roxo'], # zcav em pensamento (na real pode ser um esqueleto qualquer)
+		   [c.pit, 'Dinossauro Roxo', 'Dinossaurinho Roxo 1', 'Dinossaurinho Roxo 2', 'Dinossauro Verde'],
+		   [c.pit, 'Dinossauro Verde', 'Planta Carnívora'],
+		   [c.pit, c.fig(), c.fig(), c.thu, 'Dinossaurinho Roxo 1', 'Dinossaurinho Roxo 2', c.end], # figurantes são homens das cavernas
+		   [c.beg, c.ceb, c.mag, c.mon, c.san, c.cas, c.xav, c.end],
+		   [],
+		   [c.beg, c.ceb],
+		   [c.ceb, c.cas],
+		   [c.ceb, c.cas, c.mon, c.san],
+		   [c.ceb, c.mon, c.san],
+		   [c.ceb, c.mon, c.san],
+		   [c.ceb, 'Alien Verde - filho', 'Alien Verde - mãe', c.end], # a mãe só em voz e o filho só em mão saindo da nave
+		   [c.beg, c.nim, c.cas, c.mdoc, c.end],
+		   [],
+		   []]
