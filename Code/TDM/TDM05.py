5de5770c2c4a1f64b12b6e4bb90d623420e11ece
jvprimakipr <j.joaoviniciuspr@hotmail.com>
2021-08-31 15:53:10 -0300

    Fix TDM 25

--- Code/TDM/TDM05.py
+++ Code/TDM/TDM05.py
@@ -1,42 +1,37 @@
 from Functions.class_TM import TM
 from Functions.class_characters import characters
-
-c = characters()
-c.reset()
-c.comic_book = '#tdm05'
-c.cb = c.comic_book + ' '
-
+c = characters('#tdm_24') #Capa
 pages5 = [[c.beg, 'Homem Invisível', 'Mulher Invisível', c.cra, c.zcav, c.fra, c.lob, c.zvam, c.mum, c.pen, c.end],
           [],
           [c.pen],
-          [c.beg, c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child()],
-          [c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child()],
-          [c.child(), c.child(), c.child(), c.child(), c.child()],
-          [c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.fig()],
-          [c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child()],
-          [c.mag, c.att(), c.att(), c.shade(), c.shade(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child()],
+          [c.beg, c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid()],
+          [c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid()],
+          [c.kid(), c.kid(), c.kid(), c.kid(), c.kid()],
+          [c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.fig()],
+          [c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid()],
+          [c.mag, c.att(), c.att(), c.shade(), c.shade(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid()],
           [c.pen, c.cra],
           [c.fig(), c.pen],
-          [c.zvam, c.mum, c.fra, c.lob, c.cra, c.pen, c.zcav, c.cb + 'Camisa 1', c.cb + 'Camisa 2', c.cb + 'Camisa 3', c.cb + 'Vestido', c.cb + 'Lixo'],
-          [c.zvam, c.mum, c.fra, c.lob, c.cra, c.pen, c.zcav, c.cb + 'Vestido', c.alm, c.cb + 'Toalha', c.cb + 'Lixo', c.cb + 'Fantasminha'],
-          [c.zvam, c.fra, c.cra, c.pen, c.cb + 'Vestido', c.alm, c.cb + 'Lixo', c.cb + 'Fantasminha'],
-          [c.zvam, c.lob, c.cra, c.pen, c.cb + 'Vestido', c.alm, c.cb + 'Toalha', c.cb + 'Camisa 1', c.cb + 'Camisa 2', c.cb + 'Camisa 3', c.cb + 'Fantasminha'],
-          [c.fra, c.lob, c.cra, c.zcav, c.pen, c.cb + 'Vestido', c.alm, c.cb + 'Toalha', c.cb + 'Lixo', c.cb + 'Camisa 1', c.cb + 'Camisa 2', c.cb + 'Camisa 3', c.cb + 'Fantasminha'],
-          [c.fig(), c.zcav, c.pen, c.cb + 'Vestido', c.cb + 'Toalha', c.cb + 'Lixo', c.cb + 'Camisa 1', c.cb + 'Camisa 2', c.cb + 'Camisa 3', c.end], # fim da história
+          [c.zvam, c.mum, c.fra, c.lob, c.cra, c.pen, c.zcav, c.ID('Camisa 1'), c.ID('Camisa 2'), c.ID('Camisa 3'), c.ID('Vestido'), c.ID('Lixo')],
+          [c.zvam, c.mum, c.fra, c.lob, c.cra, c.pen, c.zcav, c.ID('Vestido'), c.alm, c.ID('Toalha'), c.ID('Lixo'), c.ID('Fantasminha')],
+          [c.zvam, c.fra, c.cra, c.pen, c.ID('Vestido'), c.alm, c.ID('Lixo'), c.ID('Fantasminha')],
+          [c.zvam, c.lob, c.cra, c.pen, c.ID('Vestido'), c.alm, c.ID('Toalha'), c.ID('Camisa 1'), c.ID('Camisa 2'), c.ID('Camisa 3'), c.ID('Fantasminha')],
+          [c.fra, c.lob, c.cra, c.zcav, c.pen, c.ID('Vestido'), c.alm, c.ID('Toalha'), c.ID('Lixo'), c.ID('Camisa 1'), c.ID('Camisa 2'), c.ID('Camisa 3'), c.ID('Fantasminha')],
+          [c.fig(), c.zcav, c.pen, c.ID('Vestido'), c.ID('Toalha'), c.ID('Lixo'), c.ID('Camisa 1'), c.ID('Camisa 2'), c.ID('Camisa 3'), c.end], # fim da história
           [c.beg, c.cas],
           [c.cas, c.mcas, c.end], # fim da história
           [c.beg, c.mon, c.mar, c.bid], # Bidu só em fala
           [c.mon, c.mar, c.bid],
           [c.mon, c.mar, c.floq],
           [c.mon, c.mar, c.moca, c.anim(), c.anim(), c.anim()],
-          [c.mon, c.mar, c.ceb, c.san, c.end], # fim da história
+          [c.mon, c.mar, c.ceb, c.san, c.end], # fim da histór)ia
           [],
           [],
-          [c.beg, c.mceb, c.ceb, c.anj, c.cb + 'Anjinho 2', c.cb + 'Anjinho 3', c.cb + 'Anjinho 4', c.cb + 'Anjinho 1', c.end], # fim da história
+          [c.beg, c.mceb, c.ceb, c.anj, c.ID('Anjinho 2'), c.ID('Anjinho 3'), c.ID('Anjinho 4'), c.ID('Anjinho 1'), c.end], # fim da história
           [c.beg, c.chi, c.anim()],
           [c.chi, c.anim(add = 0), c.end], # fim da história
-          [c.beg, c.mau, c.cb + 'Desenhista 1', c.cb + 'Desenhista 2', c.cb + 'Desenhista 3', c.cb + 'Desenhista 4', c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade()],
-          [c.mau, c.cb + 'Desenhista 1', c.cb + 'Desenhista 2', c.cb + 'Desenhista 3', c.cb + 'Desenhista 4', c.tin, c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade()], # fim da história
+          [c.beg, c.mau, c.ID('Desenhista 1'), c.ID('Desenhista 2'), c.ID('Desenhista 3'), c.ID('Desenhista 4'), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade()],
+          [c.mau, c.ID('Desenhista 1'), c.ID('Desenhista 2'), c.ID('Desenhista 3'), c.ID('Desenhista 4'), c.tin, c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade()], # fim da história
           [c.naj, c.jot, c.anim(), c.end], # fim da história
           [], # [c.dud, c.cas, 'Anjinho', c.franj, c.mon], # passatempos
           [], # [c.mon, c.mag, c.cas, c.ceb, c.bid, c.xav, c.pen], # correio
