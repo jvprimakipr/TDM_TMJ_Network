dcb4fac34500405f8bb73452a66cad20bef93522
jvprimakipr <j.joaoviniciuspr@hotmail.com>
2021-08-31 15:48:16 -0300

    Fix TDM 23

--- Code/TDM/TDM04.py
+++ Code/TDM/TDM04.py
@@ -1,39 +1,34 @@
 from Functions.class_TM import TM
 from Functions.class_characters import characters
-
-c = characters()
-c.reset()
-c.comic_book = '#tdm04'
-c.cb = c.comic_book + ' '
-
-pages4 = [[c.beg, c.cas, c.ceb, c.mon, 'Duende', c.cb + 'Múmia', c.end], # capa
+c = characters('#tdm_23') #Capa
+pages4 = [[c.beg, c.cas, c.ceb, c.mon, 'Duende', c.ID('Múmia'), c.end], # capa
           [],
           [c.mon], # introdução
           [c.beg, c.mon],
           [c.mon],
-          [c.mon, 'Duende', c.mot(), c.fat(), c.mmon, c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade()], # 19 crianças, 2 mães e 2 pais
+          [c.mon, 'Duende', c.mot(), c.fat(), c.mmon, c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade(), c.shade()], # 19 crianças, 2 mães e 2 pais
           [c.mon, 'Duende', c.mmon],
           [c.mon, 'Duende', c.mmon],
-          [c.mon, 'Duende', c.mmon, c.child(), c.child()],
-          ['Duende', c.att(), c.shade(), c.mot(), c.mot(), c.fat(), c.fat(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child()],
-          ['Duende', c.att(n = 1), c.mon, c.mmon, c.fat(), c.child(), c.att()],
+          [c.mon, 'Duende', c.mmon, c.kid(), c.kid()],
+          ['Duende', c.att(), c.shade(), c.mot(), c.mot(), c.fat(), c.fat(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid()],
+          ['Duende', c.att(n = 1), c.mon, c.mmon, c.fat(), c.kid(), c.att()],
           [c.mon, c.mmon, 'Duende'],
-          ['Duende', c.att(), c.att(), c.child(), c.child()],
+          ['Duende', c.att(), c.att(), c.kid(), c.kid()],
           ['Duende', c.mon],
-          ['Duende', c.mon, c.child(), c.child()],
-          ['Duende', c.mon, c.child(), c.ceb, c.cas],
+          ['Duende', c.mon, c.kid(), c.kid()],
+          ['Duende', c.mon, c.kid(), c.ceb, c.cas],
           ['Duende', c.mon, c.ceb, c.cas],
-          ['Duende', c.child(), c.ceb, c.cas],
+          ['Duende', c.kid(), c.ceb, c.cas],
           ['Duende', c.mon, c.att(), c.ceb, c.cas],
-          ['Duende', c.mon, c.child(), c.child(), c.ceb, c.cas],
+          ['Duende', c.mon, c.kid(), c.kid(), c.ceb, c.cas],
           ['Duende', c.mon, c.ceb, c.cas],
           ['Duende', c.mon, c.ceb, c.cas],
-          ['Duende', c.cb + 'Duende 2', c.cb + 'Duende 3', c.cb + 'Duende 4'],
-          ['Duende', c.cb + 'Duende 2', c.cb + 'Duende 3', c.cb + 'Duende 4', c.cb + 'Duende 5', 'Papai Noel'],
-          ['Duende', c.cb + 'Duende 2', c.cb + 'Duende 3', c.cb + 'Duende 4', c.cb + 'Duende 5', c.cb + 'Duende 6', c.cb + 'Duende 7', 'Papai Noel'],
-          ['Duende', c.cb + 'Duende 2', c.cb + 'Duende 3', c.cb + 'Duende 4', c.cb + 'Duende 5', 'Papai Noel'],
-          ['Duende', c.cb + 'Duende 2', c.cb + 'Duende 3', 'Papai Noel'],
-          ['Papai Noel', c.cb + 'Rena 1', c.cb + 'Rena 2', c.cb + 'Rena 3', 'Duende', c.mon, c.ceb, c.cas, c.end], # fim da história
+          ['Duende', c.ID('Duende 2'), c.ID('Duende 3'), c.ID('Duende 4')],
+          ['Duende', c.ID('Duende 2'), c.ID('Duende 3'), c.ID('Duende 4'), c.ID('Duende 5'), 'Papai Noel'],
+          ['Duende', c.ID('Duende 2'), c.ID('Duende 3'), c.ID('Duende 4'), c.ID('Duende 5'), c.ID('Duende 6'), c.ID('Duende 7'), 'Papai Noel'],
+          ['Duende', c.ID('Duende 2'), c.ID('Duende 3'), c.ID('Duende 4'), c.ID('Duende 5'), 'Papai Noel'],
+          ['Duende', c.ID('Duende 2'), c.ID('Duende 3'), 'Papai Noel'],
+          ['Papai Noel', c.ID('Rena 1'), c.ID('Rena 2'), c.ID('Rena 3'), 'Duende', c.mon, c.ceb, c.cas, c.end], # fim da história
           [],
           [c.beg, c.cas],
           [c.cas, c.spa, c.anj, c.end], # fim da história
