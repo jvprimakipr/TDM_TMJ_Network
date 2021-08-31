db0a632e07e33cdb2b42d4d4cb4d1b4f2f05f920
jvprimakipr <j.joaoviniciuspr@hotmail.com>
2021-08-31 16:06:48 -0300

    Fix TDM 27

--- Code/TDM/TDM08.py
+++ Code/TDM/TDM08.py
@@ -1,34 +1,29 @@
 from Functions.class_TM import TM
 from Functions.class_characters import characters
-
-c = characters()
-c.reset()
-c.comic_book = '#tdm08'
-c.cb = c.comic_book + ' '
-
-pages8 = [[c.beg, c.mon, c.ceb, c.cas, c.mag, c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), c.child(), 'Planeta Terra', c.end],
+c = characters('#tdm_27') #Capa
+pages8 = [[c.beg, c.mon, c.ceb, c.cas, c.mag, c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), c.kid(), 'Planeta Terra', c.end],
           [],
           [c.ceb, c.mon, c.mag, c.cas, 'Planeta Terra'],
           [c.beg, c.mag, c.mon, c.san, c.ceb],
           [c.mag, c.mon, c.san, c.ceb, c.cas],
-          [c.mag, c.mon, c.san, c.ceb, c.cas, c.shade(), c.att(), c.att(), c.child(), c.child()],
-          [c.mon, c.san, 'Planeta Terra', c.shade(), c.child(), c.child(), c.child(), c.child(), c.fig()],
-          [c.child(), c.mot(), c.mon, c.san, c.ceb, c.mag, c.mag, 'Planeta Terra'],
+          [c.mag, c.mon, c.san, c.ceb, c.cas, c.shade(), c.att(), c.att(), c.kid(), c.kid()],
+          [c.mon, c.san, 'Planeta Terra', c.shade(), c.kid(), c.kid(), c.kid(), c.kid(), c.fig()],
+          [c.kid(), c.mot(), c.mon, c.san, c.ceb, c.mag, c.mag, 'Planeta Terra'],
           [c.mon, c.san, c.ceb, c.mag, c.mag, 'Planeta Terra'],
           [c.mon, 'Planeta Terra'],
           [c.anim(), c.fig(), c.fig(), c.fig(), c.fig(), c.fig()],
           [c.mon, c.san, c.ceb, c.mag, c.mag, 'Planeta Terra'],
           ['Planeta Terra', c.mcas, c.cas, c.mceb, c.pceb, c.ceb, c.mon, c.mcas, c.pcas, c.mag, c.fig()],
-          [c.child(), c.mon, c.san, c.ceb, c.mag, c.mag, 'Planeta Terra'],
-          [c.mon, c.ceb, 'Planeta Terra', c.san, c.mag, c.cas, c.cb + 'Roteirista 1', c.cb + 'Roteirista 2', c.cb + 'Roteirista 3', c.cb + 'Roteirista 4'],
-          [c.mon, c.ceb, c.mag, c.cas, 'Planeta Terra', c.child(), c.child(), c.child(), c.child(), c.shade(), c.shade(), c.shade(), c.shade()],
+          [c.kid(), c.mon, c.san, c.ceb, c.mag, c.mag, 'Planeta Terra'],
+          [c.mon, c.ceb, 'Planeta Terra', c.san, c.mag, c.cas, c.ID('Roteirista 1'), c.ID('Roteirista 2'), c.ID('Roteirista 3'), c.ID('Roteirista 4')],
+          [c.mon, c.ceb, c.mag, c.cas, 'Planeta Terra', c.kid(), c.kid(), c.kid(), c.kid(), c.shade(), c.shade(), c.shade(), c.shade()],
           ['Planeta Terra', 'Lua', c.mon, c.ceb, c.cas, c.mag, c.end], # fim da história
           [c.beg, c.san, c.mon, c.ceb],
           [c.san, c.mon, c.ceb, c.end], # fim da história
           [],
           [c.beg, c.anj, c.ceb], # cebolinha só em balão de fala
           [c.ceb, c.san, c.mon, c.anj],
-          [c.ceb, c.mon, c.san, c.anj, c.cb + 'Anjinha', c.end], # fim da história
+          [c.ceb, c.mon, c.san, c.anj, c.ID('Anjinha'), c.end], # fim da história
           [c.beg, c.mar],
           [c.mar],
           [c.mar],
@@ -55,7 +50,7 @@ pages8 = [[c.beg, c.mon, c.ceb, c.cas, c.mag, c.child(), c.child(), c.child(), c
           [c.mum, c.zvam, c.pen, c.lob], # pensando no lobi
           [c.mum, c.zvam, c.pen, c.lob, c.fig(), c.shade(), c.end], # fim da história
           [c.beg, c.mceb, c.ceb, c.fig(), c.cas, c.end], # fim da história
-          [c.beg, c.bid, c.anim(), c.child()], # criança só em balão de fala
+          [c.beg, c.bid, c.anim(), c.kid()], # criança só em balão de fala
           [c.bid, c.anim(add = 0), c.anim(), c.fig(), c.fig()], # o segundo figurante só em balão de fala
           [c.bid, c.anim(add = 0), c.anim(), c.fig()], # figurante só em balão de fala
           [c.bid, c.anim(add = 0), c.anim(), c.fig(), c.fig(), c.fig(), c.fig(), c.fig(), c.fig(), c.fig(), c.fig(), c.end], # figurantes em balão de fala / fim da história
@@ -67,7 +62,7 @@ pages8 = [[c.beg, c.mon, c.ceb, c.cas, c.mag, c.child(), c.child(), c.child(), c
           [c.beg, c.cas, c.end], # fim da história
           [c.beg, c.cas, c.san, c.mon, 'Menino de Pedra'],
           [c.cas, c.san, 'Menino de Pedra'],
-          ['Menino de Pedra', c.child(), c.child(), c.ceb, c.san, c.mon],
+          ['Menino de Pedra', c.kid(), c.kid(), c.ceb, c.san, c.mon],
           [c.cas, c.san, c.mon, 'Menino de Pedra'],
           [c.cas, c.san, c.mon, 'Menino de Pedra', c.franj],
           [c.cas, c.san, c.mon, 'Menino de Pedra', c.franj, c.end], # fim da história
